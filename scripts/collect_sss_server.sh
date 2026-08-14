d9df81ea9309242f7ee15a837848162153b698d9
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2026-08-14 22:41:40 +0800

    更新 collect_sss_server.sh

--- scripts/collect_sss_server.sh
+++ scripts/collect_sss_server.sh
@@ -6,7 +6,7 @@ BASE=/root/.config/github-sss
 WORK="$(mktemp -d)"
 API='https://api.github.com/repos/Rnik666/-/contents/SSS'
 BRANCH='2024/3'
-MAX_NODES=160
+MAX_NODES=200
 
 trap 'rm -rf "$WORK"' EXIT
 
